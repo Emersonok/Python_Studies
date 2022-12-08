2febb31d44f719eef7eadf53b5f996590fa3ab93
Emerson Okorie <emersonokorie@icloud.com>
2022-12-08 09:58:38 -0300

    Update

--- Anagram.py
+++ Anagram.py
@@ -1,13 +1,16 @@
-def anagram(str1, str2):
-    if len(str1) != len(str2):
+def anagram(str1, str2): #create a function with two vars to  
+                         #represent both strings to be compared
+
+    if len(str1) != len(str2): # anagrams must have the same length
+                               #They're not anagrams if len are different
         return False
     
-    str1 = sorted(str1)
+    str1 = sorted(str1)     #sort both anagrams so you can compare the values 1 by 1
     str2 = sorted(str2)
     print(str1, str2)
 
-    for x in range(len(str1)):
-        if str1[x] == str2[x]:
+    for x in range(len(str1)): #loop thru one value 
+        if str1[x] == str2[x]: #compare it with the other value
             return True
         else:
             return False
