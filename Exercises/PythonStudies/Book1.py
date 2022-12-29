a4c7b26f917b1e36341338203f8b4f09943b7b20
Emerson Okorie <emersonokorie@icloud.com>
2022-12-29 02:16:07 -0300

    New Folder

---
+++ Exercises/PythonStudies/Book1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band name generator")
+City = input("What's the name of the city you grew up in? ")
+Pet = input("What's the name of your pet? ")
+print(f"Your Band name could be {City} {Pet}")
+   
