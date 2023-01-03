2050960d22596aec786bb87afaecc817269e86ee
Emerson Okorie <emersonokorie@icloud.com>
2023-01-03 10:01:28 -0300

    update

--- Projects/PasswordGen.py
+++ Projects/PasswordGen.py
@@ -4,9 +4,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+#nr_letters= int(input("How many letters would you like in your password?\n")) 
+#nr_symbols = int(input(f"How many symbols would you like?\n"))
+#nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #password = ""
 #for letter in range(1, nr_letters + 1): 
@@ -20,24 +20,54 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #print(password)
 
-password = []
+#password = []
 
-for letter in range(1, nr_letters + 1):
-    password += random.choice(letters)
+#for letter in range(1, nr_letters + 1):
+ #   password += random.choice(letters)
 
-for symbol in range(1, nr_symbols + 1):
-    password.append(random.choice(symbols))
+#for symbol in range(1, nr_symbols + 1):
+ #   password.append(random.choice(symbols))
 
-for number in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
+#for number in range(1, nr_numbers + 1):
+ #   password += random.choice(numbers)
 
-random.shuffle(password)
+#random.shuffle(password)
 
-final_password = ""
-for word in password:
-    final_password += word
+#final_password = ""
+#for word in password:
+ #   final_password += word
 
-print(final_password)
+#print(final_password)
+
+select = input("Write 'Yes' to generate your password automatically,\nWrite 'No' to enter your password ")
+select = select.upper()
+password = [] #password a list so it can be shuffled
+if select == "NO":
+    enter = input("Enter your password ")
+    print(f"Your password is: {enter}")
+
+elif select == "YES":
+    for letter in range(1, 5):
+      password += random.choice(letters)
+
+    for symbol in range(1, 4):
+        password += random.choice(symbols)
+
+    for number in range(1, 4):
+        password += random.choice(numbers)
+
+    random.shuffle(password)
+    
+    final_password = "" #change password back to string after shuffling
+    for word in password:
+        final_password += word
+
+    print(f"Your password is {final_password}")
+    
+
+
+
+
       
 
 
